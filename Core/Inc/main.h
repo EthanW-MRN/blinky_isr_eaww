ee199e779307a37ef63d1d9fa2d4e585c88e7a90
EthanW-MRN <ethanawatkins@icloud.com>
2026-09-23 15:45:01 -0400

    Update main.h

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -100,7 +100,7 @@ void Error_Handler(void);
 #endif
 
 /* USER CODE BEGIN Private defines */
-#define DELAY_COUNT 1000000
+#define DELAY_COUNT 676767
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
